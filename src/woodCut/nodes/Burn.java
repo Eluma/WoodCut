df77bff78939c9d22320476f03994bd32ee36e90
Eluma <guardian_cm@hotmail.com>
2013-05-10 23:51:37 +0800

    Added dynamic timer

--- src/woodCut/nodes/Burn.java
+++ src/woodCut/nodes/Burn.java
@@ -3,7 +3,7 @@ package woodCut.nodes;
 import org.powerbot.core.script.job.state.Node;
 import org.powerbot.game.api.methods.node.SceneEntities;
 import org.powerbot.game.api.methods.tab.Inventory;
-import org.powerbot.game.api.util.Random;
+import org.powerbot.game.api.util.Timer;
 import org.powerbot.game.api.wrappers.node.SceneObject;
 
 import woodCut.misc.Variables;
@@ -21,25 +21,42 @@ public class Burn extends Node {
 	{
 		Variables.status="BURN!";
 		
-		final SceneObject fire = SceneEntities.getNearest(Variables.fireId);
-		
+		SceneObject fire = SceneEntities.getNearest(Variables.fireId);
+		Timer t = new Timer(140000); 
 		if(fire == null)
-			if(Inventory.getItem(Variables.logId).getWidgetChild().interact("Light"))//light one prevent running far away if others set a fire.
-			{
-				sleep(Random.nextInt(3000,4500));
-
-			}
+			fire = lightFire();
 		
 		if(Inventory.getItem(Variables.logId).getWidgetChild().interact("Use"))
 		{
 			if(fire.interact("Use"))
-				sleep(Random.nextInt(2000, 3000)*Inventory.getCount(Variables.logId));
-
+				while(t.isRunning())
+				{
+					fire = SceneEntities.getNearest(Variables.fireId);
+					if(fire == null)
+						break;
+					else if(Inventory.getCount(Variables.logId)==0)
+						break;
+						sleep(2000);
+				}
 		}
 		
-		
-		
 	}
-
+	
+	public SceneObject lightFire()
+	{
+		SceneObject fire = SceneEntities.getNearest(Variables.fireId);
+		Timer t = new Timer(10000);
+		if(Inventory.getItem(Variables.logId).getWidgetChild().interact("Light"))//light one prevent running far away if others set a fire.
+		{	
+			while(t.isRunning())
+			{
+				fire = SceneEntities.getNearest(Variables.fireId);
+				if(fire != null)
+					break;
+			}
+			
+		}
+		return fire;
+	}
 
 }
